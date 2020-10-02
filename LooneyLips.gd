a7c63a3d9ccb847fe226e27db35b86b1cf59d98b
Kevin Millard <kaz101130@gmail.com>
2020-10-01 21:49:21 -0400

    update

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -1,30 +1,36 @@
 extends Control
+
 var player_words = []
 var prompts = ['Person','noun','adjective','adverb']
 var story = '%s drank a %s and thought it was the %s thing he had ever %s.'
+
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
 onready var StoryBox = $VBoxContainer/Story 
 
 func _ready():
 	welcome_screen()
 
-func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
-
-func update_DisplayText(words):
-	print(words)
-	StoryBox.text = words
-	PlayerText.clear()
-
-func _on_Button_pressed():
-	append_word_list()
-	PlayerText.clear()
-	prompt_user()
-
 func welcome_screen():
 	StoryBox.text = "Welcome to Looney Lips the Mad Libs rip off extrordinare!! Press button to begin"
+
+func _on_StartButtom_pressed():
+	_on_Button_pressed()
 	
-	
+func _on_PlayerText_text_entered(new_text):
+	_on_Button_pressed()
+
+func _on_Button_pressed():
+	if player_words.size() < prompts.size():
+		if PlayerText.text:
+			append_word_list()
+		if player_words.size() == prompts.size():
+			tell_story()
+			PlayerText.clear()
+			player_words = []
+		else:
+			prompt_user()
+			PlayerText.clear()
+			
 func prompt_user():
 	StoryBox.text = 'Alright, give me a ' + prompts[player_words.size()]
 	
@@ -32,10 +38,5 @@ func append_word_list():
 	player_words.append(PlayerText.text)
 	
 func tell_story():
-	pass
-
-
-
-func _on_StartButtom_pressed():
-	update_DisplayText(PlayerText.text)
-	prompt_user()
+	StoryBox.text = story % player_words
+	
